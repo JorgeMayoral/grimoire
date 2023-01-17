c686068259d2d9c2002d5bb58033367fe435a916
Jorge Mayoral Álvarez <mayoralalvareszj@gmail.com>
2023-01-17 18:43:20 +0100

    feat(frontend): add editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import Editor, { Monaco } from '@monaco-editor/react';
+import { useRef } from 'react';
+import { editor as monacoEditor } from 'monaco-editor';
+
 function App() {
-  return <h1>Hello World</h1>;
+  const editorRef = useRef<monacoEditor.IStandaloneCodeEditor>();
+
+  const handleEditorMount = (
+    editor: monacoEditor.IStandaloneCodeEditor,
+    _: Monaco,
+  ) => {
+    editorRef.current = editor;
+  };
+
+  return (
+    <Editor
+      height="90vh"
+      defaultLanguage="markdown"
+      onMount={handleEditorMount}
+    />
+  );
 }
 
 export default App;
