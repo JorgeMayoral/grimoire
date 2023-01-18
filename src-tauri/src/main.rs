32533b52d544f70915f407e73839154aa991fd15
Jorge Mayoral Álvarez <mayoralalvareszj@gmail.com>
2023-01-18 19:01:56 +0100

    feat(project): add file management feature

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,75 @@
     windows_subsystem = "windows"
 )]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use std::{fs, io::Write, sync::Mutex};
+use tauri::Manager;
+
+struct EditorContent {
+    content: Mutex<String>,
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn sync_editor_content(state: tauri::State<EditorContent>, content: String) {
+    *state.content.lock().unwrap() = content;
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(EditorContent {
+            content: Default::default(),
+        })
+        .menu(build_menu())
+        .on_menu_event(handle_menu_event)
+        .invoke_handler(tauri::generate_handler![sync_editor_content])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn build_menu() -> tauri::Menu {
+    let save_item = tauri::CustomMenuItem::new("save_file", "Save File");
+    let load_item = tauri::CustomMenuItem::new("load_file", "Load File");
+    let file_submenu = tauri::Menu::with_items([save_item.into(), load_item.into()]);
+    let menu = tauri::Menu::new().add_submenu(tauri::Submenu::new("File", file_submenu));
+    menu
+}
+
+fn handle_menu_event(event: tauri::WindowMenuEvent) {
+    match event.menu_item_id() {
+        "load_file" => handle_load_file(event.window().to_owned()),
+        "save_file" => handle_save_file(event.window().to_owned()),
+        _ => {}
+    }
+}
+
+fn handle_save_file(window: tauri::Window) {
+    let state = window.state::<EditorContent>();
+    let content = state.content.lock().unwrap().clone();
+    tauri::api::dialog::FileDialogBuilder::new()
+        .add_filter("Markdown File", &["md"])
+        .add_filter("All Files", &["*"])
+        .set_title("Save File")
+        .save_file(move |file_path| {
+            let path = match file_path {
+                Some(p) => p,
+                None => return,
+            };
+            let mut file = fs::File::create(path).expect("error creating file");
+            file.write_all(content.as_bytes())
+                .expect("error writing file");
+        })
+}
+
+fn handle_load_file(window: tauri::Window) {
+    tauri::api::dialog::FileDialogBuilder::new()
+        .add_filter("Markdown File", &["md"])
+        .add_filter("All Files", &["*"])
+        .set_title("Load File")
+        .pick_file(move |file_path| {
+            let path = match file_path {
+                Some(p) => p,
+                None => return,
+            };
+            let content = fs::read_to_string(path).expect("error loading file");
+            window.emit("load_file", content).unwrap();
+        })
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import Editor, { Monaco } from '@monaco-editor/react';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { editor as monacoEditor } from 'monaco-editor';
+import { event, tauri } from '@tauri-apps/api';
 
 function App() {
+  const [content, setContent] = useState<string>('');
+
+  useEffect(() => {
+    let fileLoadUnlisten: Function = () => {};
+    event
+      .listen<string>('load_file', (ev) => {
+        setContent(ev.payload);
+      })
+      .then((fn) => (fileLoadUnlisten = fn));
+
+    return () => {
+      fileLoadUnlisten();
+    };
+  }, []);
+
   const editorRef = useRef<monacoEditor.IStandaloneCodeEditor>();
 
   const handleEditorMount = (
@@ -12,11 +28,20 @@ function App() {
     editorRef.current = editor;
   };
 
+  const handleChange = (value: string | undefined) => {
+    if (value !== undefined) {
+      setContent(value);
+      tauri.invoke('sync_editor_content', { content: value });
+    }
+  };
+
   return (
     <Editor
       height="90vh"
       defaultLanguage="markdown"
       onMount={handleEditorMount}
+      value={content}
+      onChange={handleChange}
     />
   );
 }
