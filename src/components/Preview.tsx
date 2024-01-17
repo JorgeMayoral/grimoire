7f9972ae20b7bea435cceef2ee58dd522fc1ad69
Jorge Mayoral Álvarez <jorge.ma@yorch.dev>
2024-01-17 19:32:09 +0100

    fix: :lipstick: center preview

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -6,8 +6,8 @@ export const Preview = () => {
 	const { content } = useContentStore((state) => state);
 
 	return (
-		<div className="p-4">
-			<Markdown className="prose" remarkPlugins={[remarkGfm]}>
+		<div className="flex justify-center p-4">
+			<Markdown className="prose w-full" remarkPlugins={[remarkGfm]}>
 				{content}
 			</Markdown>
 		</div>
